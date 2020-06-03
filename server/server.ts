0c8f3fc686586e459bb0708f4c08ca2a3a680fee
Zonarius <zonarius90@gmail.com>
2020-06-03 11:48:53 +0200

    Add API Server

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,23 @@
 import express from 'express';
+import * as body from 'body-parser';
+import BillaClient from './billa-client/client';
 
+const billa = new BillaClient();
 const app = express();
+const api = express.Router();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+api.use(body.json());
+
+api.get("/searchByUrl", async (req, res) => {
+  const response = await billa.searchByUrl(req.body.url);
+  res.send(response);
+})
+
+api.get("/infos/:id", async (req, res) => {
+  const response = await billa.articleInfos(req.params.id);
+  res.send(response);
+})
+
+app.use("/api", api);
 
 app.listen(8080, () => console.log("Running on port 8080"));
